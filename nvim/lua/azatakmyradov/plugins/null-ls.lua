bba0686f54bd7c70af5c19f4bfd1f09895323405
Azat Akmyradov <azat@akmyradov.me>
2023-09-16 23:12:18 -0400

    Fix for a tab history annoying bug

--- nvim/lua/azatakmyradov/plugins/null-ls.lua
+++ nvim/lua/azatakmyradov/plugins/null-ls.lua
@@ -2,7 +2,7 @@ local null_ls = require("null-ls")
 
 local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
 local event = "BufWritePost" -- or "BufWritePre"
-local async = event == "BufWritePost"
+local async = event == "BufWritePre"
 
 null_ls.setup({
     on_attach = function(client, bufnr)
